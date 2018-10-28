7eec3a2b10e0430b2c59057a7f3de6ca201e8c1b
Michael Nix <nix@mail.hs-ulm.de>
2018-10-28 11:44:18 +0100

    Enemys can die, Enemy Sprite reworked, Death Animation Images added

--- Assets/Bullet/Bullet.cs
+++ Assets/Bullet/Bullet.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
-    public float speed = 50f;
+    public float speed = 25f;
     public Rigidbody2D rb;
-
+    public int damage = 50;
 
 	void Start () {
         rb.velocity = transform.right * speed;
@@ -14,7 +14,11 @@ public class Bullet : MonoBehaviour {
 	
     void OnTriggerEnter2D (Collider2D hitInfo)
     {
-        Debug.Log(hitInfo.name);
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
         Destroy(gameObject);
     }
 }
---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int health = 100;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0 || true)        //currently Enemys should always die on 1 hit
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //Instantiate(deathEffect, transform.position, Quaternion.identity);        // TODO
+        Destroy(gameObject);
+    }
+}
