cecf38923c3f8d7968f927cd42ad7a8852ec44c5
Michael Nix <nix@mail.hs-ulm.de>
2018-11-01 20:02:14 +0100

    Character can die from enemys

---
+++ Assets/Character/Character.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D hitInfo){
+        if (hitInfo.gameObject.GetComponent<Enemy>())
+            Destroy(gameObject);
+    }
+}
